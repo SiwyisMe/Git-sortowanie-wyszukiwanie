75664fb3e6e6807d606a7b084a589c4d314a0d2b
Siwy - Filip Siwińki <dev.siwy@gmail.com>
2024-02-12 16:42:58 +0100

    algorytmy.cpp + poprawki do main.cpp - komentarz i wielkosc tablicy

---
+++ algorytmy.cpp
@@ -0,0 +1,50 @@
+#include "algorytmy.hpp"
+#include <iostream>
+
+void wypelnij_tablice(int t[], int size) {
+    //  inplementacja  
+}
+
+void wydrukuj_tablice(int t[], int size) {
+    //  inplementacja  
+}
+
+void sortowanie_babelkowe(int t[], int size) {
+    //  inplementacja 
+}
+
+void sortowanie_przez_scalanie(int t[], int size) {
+    //  inplementacja 
+}
+
+void sortowanie_szybkie(int t[], int size) {
+    //  inplementacja 
+}
+
+void sortowanie_przez_wstrzasanie(int t[], int size) {
+    //  inplementacja 
+}
+
+void sortowanie_przez_wstawianie(int t[], int size) {
+    //  inplementacja 
+}
+
+int wyszukiwanie_binarne(int t[], int size, int key) {
+    //  inplementacja 
+}
+
+int wyszukiwanie_liniowe(int t[], int size, int key) {
+    //  inplementacja 
+}
+
+int wyszukiwanie_liniowe_z_wart(int t[], int size, int key) {
+    //  inplementacja  
+}
+
+int wyszukiwanie_interpolacyjne(int t[], int size, int key) {
+    //  inplementacja 
+}
+
+int wyszukiwanie_skokowe(int t[], int size, int key) {
+    //  inplementacja 
+}
--- main.cpp
+++ main.cpp
@@ -1,52 +1,53 @@
 #include <iostream>
 #include "algorytmy.hpp"
 
+const int SIZE = 10;  // rozmiar tablicy
+
 int main() {
     int t[SIZE];
     wypelnij_tablice(t, SIZE);
-    wydrukuj_tablice();
+    wydrukuj_tablice(t, SIZE);
     sortowanie_babelkowe(t, SIZE);
-    wydrukuj_tablice();
+    wydrukuj_tablice(t, SIZE);
 
-    wypelnij_tablice();
-    wydrukuj_tablice();
+    wypelnij_tablice(t, SIZE);
+    wydrukuj_tablice(t, SIZE);
     sortowanie_przez_scalanie(t, SIZE);
-    wydrukuj_tablice();
+    wydrukuj_tablice(t, SIZE);
 
-    wypelnij_tablice();
-    wydrukuj_tablice();
+    wypelnij_tablice(t, SIZE);
+    wydrukuj_tablice(t, SIZE);
     sortowanie_szybkie(t, SIZE);
-    wydrukuj_tablice();
+    wydrukuj_tablice(t, SIZE);
 
-    wypelnij_tablice();
-    wydrukuj_tablice();
+    wypelnij_tablice(t, SIZE);
+    wydrukuj_tablice(t, SIZE);
     sortowanie_przez_wstrzasanie(t, SIZE);
-    wydrukuj_tablice();
+    wydrukuj_tablice(t, SIZE);
 
-    wypelnij_tablice();
-    wydrukuj_tablice();
-    sortowanie_przez_wstawianie(t, SIZE, t_out);
-    wydrukuj_tablice();
-
+    wypelnij_tablice(t, SIZE);
+    wydrukuj_tablice(t, SIZE);
+    sortowanie_przez_wstawianie(t, SIZE);
+    wydrukuj_tablice(t, SIZE);
 
     int tt[SIZE] = {};
-    int k_exist_in_tt = x;
-    int k_not_exist_in_tt = y;
+    int k_exist_in_tt = 42;  // liczba jaka chce
+    int k_not_exist_in_tt = 99;  // liczba jaka chce
 
-    std::cout << wyszukiwanie_binarne(tt, SIZE, k_exist_in_tt);
-    std::cout << wyszukiwanie_binarne(tt, SIZE, k_not_exist_in_tt);
+    std::cout << wyszukiwanie_binarne(tt, SIZE, k_exist_in_tt) << std::endl;
+    std::cout << wyszukiwanie_binarne(tt, SIZE, k_not_exist_in_tt) << std::endl;
 
-    std::cout << wyszukiwanie_liniowe(tt, SIZE, k_exist_in_tt);
-    std::cout << wyszukiwanie_liniowe(tt, SIZE, k_not_exist_in_tt);
+    std::cout << wyszukiwanie_liniowe(tt, SIZE, k_exist_in_tt) << std::endl;
+    std::cout << wyszukiwanie_liniowe(tt, SIZE, k_not_exist_in_tt) << std::endl;
 
-    std::cout << wyszukiwanie_liniowe_z_wart(tt, SIZE, k_exist_in_tt);
-    std::cout << wyszukiwanie_liniowe_z_wart(tt, SIZE, k_not_exist_in_tt);
+    std::cout << wyszukiwanie_liniowe_z_wart(tt, SIZE, k_exist_in_tt) << std::endl;
+    std::cout << wyszukiwanie_liniowe_z_wart(tt, SIZE, k_not_exist_in_tt) << std::endl;
 
-    std::cout << wyszukiwanie_interpolacyjne(tt, SIZE, k_exist_in_tt);
-    std::cout << wyszukiwanie_interpolacyjne(tt, SIZE, k_not_exist_in_tt);
+    std::cout << wyszukiwanie_interpolacyjne(tt, SIZE, k_exist_in_tt) << std::endl;
+    std::cout << wyszukiwanie_interpolacyjne(tt, SIZE, k_not_exist_in_tt) << std::endl;
 
-    std::cout << wyszukiwanie_skokowe(tt, SIZE, k_exist_in_tt);
-    std::cout << wyszukiwanie_skokowe(tt, SIZE, k_not_exist_in_tt);
+    std::cout << wyszukiwanie_skokowe(tt, SIZE, k_exist_in_tt) << std::endl;
+    std::cout << wyszukiwanie_skokowe(tt, SIZE, k_not_exist_in_tt) << std::endl;
 
     return 0;
 }
