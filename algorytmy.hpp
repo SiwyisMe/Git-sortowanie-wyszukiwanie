9d4c656b804f576d386e0dedefb949bf2e570b99
Siwy - Filip Siwińki <dev.siwy@gmail.com>
2024-02-12 17:05:52 +0100

    algorytmy.hpp deklaracje funkcji

---
+++ algorytmy.hpp
@@ -0,0 +1,13 @@
+void wypelnij_tablice(int t[], int size);
+void wydrukuj_tablice(int t[], int size);
+void sortowanie_babelkowe(int t[], int size);
+void sortowanie_przez_scalanie(int t[], int size);
+void sortowanie_szybkie(int t[], int size);
+void sortowanie_przez_wstrzasanie(int t[], int size);
+void sortowanie_przez_wstawianie(int t[], int size);
+
+int wyszukiwanie_binarne(int t[], int size, int key);
+int wyszukiwanie_liniowe(int t[], int size, int key);
+int wyszukiwanie_liniowe_z_wart(int t[], int size, int key);
+int wyszukiwanie_interpolacyjne(int t[], int size, int key);
+int wyszukiwanie_skokowe(int t[], int size, int key);
